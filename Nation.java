6dbc0fa50d549751e938a56a19a8595774dc3856
iyert <iyert@moravian.edu>
2021-03-18 11:13:25 -0400

    Refactored Nation.java. Marked a possible bug in the constructor.

--- Nation.java
+++ Nation.java
@@ -2,63 +2,74 @@ import java.util.ArrayList;
 
 public class Nation
 {
-    private int nationLifePoints;
-    private String nationName;
-    private ArrayList<Tribe> tribes = new ArrayList<>();
-    private ArrayList<People> population = new ArrayList<>();
-    private ArrayList<People> livingPopulation = new ArrayList<>();
+    private int lifePoints;
+    private final String name;
+    private final ArrayList<Tribe> tribes;
+    private final ArrayList<People> population;
+    private final ArrayList<People> livingPopulation;
 
     public Nation(String name, int lifePoints)
     {
-        nationName = name;
-        nationLifePoints = lifePoints;
-        for(int i = 0; i < 6; i++)
-        {
-            this.tribes.add(new Tribe(nationName, "Tribe" + i, nationLifePoints / 6));
-        }
-        population.addAll(getNationPopulation());
-        livingPopulation.addAll(population);
+        this.name = name;
+        this.lifePoints = lifePoints;
+        this.tribes = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++)
+            this.tribes.add(new Tribe(this.name, "Tribe" + i, this.lifePoints / 6));
+
+        livingPopulation = new ArrayList<>();
+        recalculateLifePointsAndLivingPopulation();
+
+        population = new ArrayList<>(livingPopulation);
+        livingPopulation.addAll(population); // Is this a bug?
+    }
+
+    public String getName()
+    {
+        return name;
     }
 
     public ArrayList<People> getNationPopulation()
     {
-        nationLifePoints = 0;
-        livingPopulation.clear();
-        for(int tribe = 0; tribe < this.tribes.size(); tribe++)
-        {
-            if(tribes.get(tribe).isTribeAlive())
-            {
-                livingPopulation.addAll(tribes.get(tribe).getLivingTribeMembers());
-                nationLifePoints += tribes.get(tribe).getTribeLifePoints();
-            }
-        }
+        recalculateLifePointsAndLivingPopulation();
         return livingPopulation;
     }
 
-    public String getNationName()
+    private void recalculateLifePointsAndLivingPopulation()
     {
-        return nationName;
+        lifePoints = 0;
+        livingPopulation.clear();
+
+        for (Tribe value : this.tribes)
+        {
+            if (value.isTribeAlive())
+            {
+                livingPopulation.addAll(value.getLivingTribeMembers());
+                lifePoints += value.getTribeLifePoints();
+            }
+        }
     }
 
     public void printTribesStatus()
     {
-        for(int tribe = 0; tribe < 1; tribe++)
+        for (Tribe tribe : tribes)
         {
-            if(tribes.get(tribe).isTribeAlive())
+            if (tribe.isTribeAlive())
             {
-                System.out.print(tribes.get(tribe).getTribeName() + " is alive and has ");
-                System.out.println(tribes.get(tribe).getTribeSize() + " members.");
+                System.out.println(
+                        tribe.getTribeName() + " is alive and has " +
+                        tribe.getTribeSize() + " members.");
             }
             else
             {
-                System.out.println(tribes.get(tribe).getTribeName() + " is dead.");
+                System.out.println(tribe.getTribeName() + " is dead.");
             }
         }
     }
 
     public String toString()
     {
-        StringBuilder result = new StringBuilder(nationName);
+        StringBuilder result = new StringBuilder(name);
         for (Tribe tribe : tribes)
             result.append('\n').append(tribe.toString());
         return result.append('\n').toString();
