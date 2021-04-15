1a09fb4b3833e2710e30f619eda0b249faf75efb
schumanm <78161232+schumanm@users.noreply.github.com>
2021-04-15 10:22:34 -0400

    Add files via upload

---
+++ GUI/src/GUIv2.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class GUIv2 {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+
+    public static void main(String[] args) throws InterruptedException {
+        //TODO
+        // Fix string library so it appears when GUI runs
+
+//         String[] nations = new String[]{"Max's Nation", "Justin's Nation",
+//                 "Elizabeth's Nation", "Tanishq's Nation"};
+
+
+        Scanner scan = new Scanner(System.in);
+
+//         System.out.println(nations);
+
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("═══════════════════░▒░══════════════════" + "\t\t\t\t\t______██████████████");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("════════════▒▒▒▓███████████▓▒═══════════" + "\t\t\t\t\t-____██▓▓▓▓▓▓▓▓▓ M ▓████");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("════════▒██████████▓▓▓▓██▓████▓═════════" + "\t\t\t\t\t-__██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("══════▒███████▓█▓▓▓█▓█▓███▓▓▓███════════" + "\t\t\t\t\t -__██████░░░░██░░██████");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("═════▓███▓██▓▓████▓██▓█▓▓▓███▓██▒═══════" + "\t\t\t\t\t██░░░░████░░██░░░░░░░░██");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("═════▓██▓███▓█▓████████▓▓█████▓██═══════" + "\t\t\t\t\t██░░░░████░░░░██░░░░░░██");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("═════▒█████████▓▓▓███████████████▒══════" + "\t\t\t\t\t-__████░░░░░░██████████");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("═════▒██████▓█████▒▒▒▒▒██████████▒══════" + "\t\t\t\t\t-__██░░░░░░░░░░░░░██");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("═════▒███████▓█▓█▒═════░▒▓█▓▓▒▒██▒══════" + "\t\t\t\t\t_____██░░░░░░░░░██");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("═════▒█████████▓█▓░░▒▒░░░░░▒▒▒▒██▒══════" + "\t\t\t\t\t-______██░░░░░░██");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("═════▒████████████▒░▒▒▒▒▒▒▒▒▒▒▒▓█▒══════" + "\t\t\t\t\t-____██▓▓████▓▓▓█");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("═════▒██████████▓▓▓▒▒▒▒▒▒▒▒▒▒▒░▓█▒══════" + "\t\t\t\t\t-_██▓▓▓▓▓▓████▓▓█");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("═════▒███████▓███▓▒▒▒▒▒▒▒▒▒▒▒▒░▒▓▒══════" + "\t\t\t\t\t██▓▓▓▓▓▓███░░███░");
+        System.out.println("═════▓█▒░░▓██▓█▓▒▒▒▒░░░░░░░░░░░▒█▒══════" + "\t\t\t\t\t-__██░░░░░░███████");
+        System.out.println("═════▓█▒▒▒░▒███▒░░▒░░░▒▒▒▒▒█▓█████══════" + "\t\t\t\t\t-____██░░░░███████");
+        System.out.println("═════██░▒██▒▒████▒░▓██████████████══════" + "\t\t\t\t\t-______██████████");
+        System.out.println("═════▒█▒▒▒▓▒░▓█▓███████████▒▒█████══════" + "\t\t\t\t\t-_____██▓▓▓▓▓▓▓▓▓██");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("══════▒█═░░▒▒▒█▒▒▒▒█▓██████░═████═══════" + "\t\t\t\t\t-_____█████████████");
+        System.out.println("═▒▓███▓██░░▒▒▒▒▒▒▒▒██████▓▒░░▒██▒═══════" + "\t\t\t\t\t\tTHE CLERIC");
+        System.out.println("██████████▓█▒▒▒▒▒▒▒▒█▓▒▒▒▒░░░═░█════════");
+        System.out.println("█████████▓▓█▓▒▒▒▒▒▓▒▒▒▒▒▒▒▓▓▒░▒█════════");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("██████████▒▒▓▓▒▒▒▒▒▒▒▒▒▒░▒▓██▓▒█════════" + "\t\t\t\t\t▒▒▒▒▒▒▒▒▒▒▒▄▄▄▄░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t────────▄▄▄▄▄▄▄▄▄");
+        System.out.println("▓▓█▓████▓▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▓▒▒▒▓▓▓██═══════" + "\t\t\t\t\t▒▒▒▒▒▒▒▒▒▄██████▒▒▒▒▒▄▄▄█▄▒▒▒▒▒▒▒▒▒▒" + "\t────────▌▐░▀░▀░▀▐");
+        System.out.println("██▓▒██▒░░▒▓▒▒▒▓█▓▒▒▒▒▒▒█████▒▒▓███══════" + "\t\t\t\t\t▒▒▒▒▒▒▒▄██▀░░▀██▄▒▒▒▒████████▄▒▒▒▒▒▒" + "\t────────▌░▌░░░░░▐");
+        System.out.println("█▓▒█▓░░▒▒▒▒▒▒▒▒▒▓▓▒▒▓▒▒░░░▒▒▒▒█████═════" + "\t\t\t\t\t▒▒▒▒▒▒███░░░░░░██▒▒▒▒▒▒█▀▀▀▀▀██▄▄▒▒▒" + "\t────────▌░░░░░░░▐");
+        System.out.println("▓▓██░░▒▒▒▒▒▒▒▒▓▒▒▓▓▓▒▒▒▒▒▓██▓▓██████════" + "\t\t\t\t\t▒▒▒▒▒▄██▌░░░░░░░██▒▒▒▒▐▌▒▒▒▒▒▒▒▒▀█▄▒" + "\t────────▌░░░░░░░▐");
+        System.out.println("▓██▒░▒▒▒▒▒▒▒▒▒▓▓▓▓▓██▓▒▒▒░▒░▒█████▓██═══" + "\t\t\t\t\t▒▒▒▒▒███░░▐█░█▌░██▒▒▒▒█▌▒▒▒▒▒▒▒▒▒▒▀▌" + "\t────────▄▄▄▄▄▄▄▄▄");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("▓▓█▒░▒▒▒▒▒▒▓▒▒▒▓▓██████▓▒▒▒▒█████▓▓██░══" + "\t\t\t\t\t▒▒▒▒████░▐█▌░▐█▌██▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t──▄▀▀▀▀▀▌▄█▄░▄█▄▐▀▀▀▀▀▄");
+        System.out.println("█▓█▒░▒▒▒▒▒▒▒▒▒▓▓▓▓████████▓▒████▓██▓██══" + "\t\t\t\t\t▒▒▒▐████░▐░░░░░▌██▒▒▒█▌▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t─█▒▒▒▒▒▐░░░░▄░░░░▌▒▒▒▒▒█");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("▓██▒░▒░▒░░░▒▒▒▒▒▒▒▓▓████▓▒▒▒███▓▓█▓▓▓█══" + "\t\t\t\t\t▒▒▒▒████░░░▄█░░░██▒▒▐█▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t▐▒▒▒▒▒▒▒▌░░░░░░░▐▒▒▒▒▒▒▒▌");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("▒██░░░░░░░░▒▒▒▒▒▒▒▓▓███▓▓▒▒██▓█▓█▓▒█▓██░" + "\t\t\t\t\t▒▒▒▒████░░░██░░██▌▒▒█▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t▐▒▒▒▒▒▒▒█░▀▀▀▀▀░█▒▒▒▒▒▒▒▌");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("▒█▒═░░░░░▒▒░▒▒▒▒▒▒▒▒▒▒▒▓▒▒██▓▓▒█▒▒█▓▒▒▓█" + "\t\t\t\t\t▒▒▒▒████▌░▐█░░███▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t▐▒▒▒▒▒▒▒▒█▄▄▄▄▄█▒▒▒▒▒▒▒▒▌");
+        System.out.println("▒█▓░░░░░░░▒▒░▒▒▒▒░░░░▒▒░░██▒▓▒▓▒▒█▓▒▒▒▒█" + "\t\t\t\t\t▒▒▒▒▐████░░▌░███▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t▐▒▒▒▒▐▒▒▒▒▒▒▒▒▒▒▒▒▐▒▒▒▒▒▌");
+        System.out.println("▒▒█▒═░░░░░░░░░░░░░░░▒░═▒█▒▒▓▒▓█▓▓▒▒▒▒▒▒▓" + "\t\t\t\t\t▒▒▒▒▒████░░░███▒▒▒▒█▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t▐▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▌");
+        System.out.println("█▒█▒═░░░░░░░░░░░░░░▒═░██▒▒▒▓▓▒▒▓▒▒▒▒▒▓▒▓" + "\t\t\t\t\t▒▒▒██████▌░████▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t▐▒▒▒▒▒▐▒▒▒▒▒▒▒▒▒▒▒▌▒▒▒▒▒▌");
+        System.out.println("▓▓▒█░░░░░░░░░░░░░░░═▒█▒▒██▓▒▒▓▒▒▒▒▒▓▓▓▒▓" + "\t\t\t\t\t▒▐████████████▒▒███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t▐▒▒▒▒▒▒▌▒▒▒▒▒▒▒▒▒▐▒▒▒▒▒▒▌");
+        System.out.println("▓█▒█▒═░░░░░░░░▒░░░═▒█▒▒▓▓▓▓▓▒▒▓▒▒▓▓▓▓▒▓▓" + "\t\t\t\t\t▒█████████████▄████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t▐▒▒▒▒▒▒▌▄▄▄▄▄▄▄▄▄▐▒▒▒▒▒▒▌");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("▓█▓▒▒═░░░░░▒░▒░░▒═▒█▒▓▓▓▓▓█▓▒▒█▓▒▓▓▓▒▓▓▒" + "\t\t\t\t\t██████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t▐▄▄▄▄▄▄▌▌███████▌▐▄▄▄▄▄▄▌");
+        System.out.println("▓▓█▓▒░░░░░░░▒▒▒▒═░█▓▓▓▓▓▓▓▓▓▓▒▓██▒▒▒▓▓█▓" + "\t\t\t\t\t██████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t─█▀▀▀▀█─▌███▌███▌─█▀▀▀▀█");
+        System.out.println("▓▓▓▓█▒═░░░░▒▒▒▒═░▓▓▓▓▓▒▓███▒▓█▒▒▓█▓▒▒▒▒▒" + "\t\t\t\t\t█████████████████▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t─▐░░░░▌─▌███▌███▌─▐░░░░▌");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("▓▓▒▓█▓═░░▒▒░▒▒═░██▓▓▓▓▓████▒▓▓██▓▓██▓▓▓▓" + "\t\t\t\t\t█████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t──▀▀▀▀──▌███▌███▌──▀▀▀▀");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("▓▒▒▓▒█░░▒▒░▒░═▒█████████▓███▓▓▓▓▓▓▓▓████" + "\t\t\t\t\t████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t────────▌███▌███▌");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("▓█▓▓▓█▓═▒░▒░░██████████████▓████████████" + "\t\t\t\t\t████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "\t────────▌███▌███▌");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("▓█▓▓▓▒█░░▒═░██████▓█▓█▓█▓▓▓▒█▒▒▒▒▒██████" + "\t\t\t\t\t\t\tTHE DEATH ARTIFACT" + "\t\t\t\t──────▐▀▀▀██▌█▀▀▀▌" +
+                "\n\t\t\tTHE WARRIOR"
+        );
+
+        System.out.println(ANSI_RED +"\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\t\t WARRING NATIONS" + ANSI_RESET);
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t IT IS NOW TIME TO PICK YOUR NATION!");
+        TimeUnit.MILLISECONDS.sleep(250);
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t PICK ONE OF THE NATIONS BELOW TO START THE GAME");
+        System.out.println(ANSI_GREEN + "\n\n\n\t\t\t\t\t\t\t\t\t\t\t Max's Nation" + ANSI_RESET);
+        System.out.println(ANSI_BLUE + "\t\t\t\t\t\t\t\t\t\t\t Justin's Nation" + ANSI_RESET);
+        System.out.println(ANSI_PURPLE + "\t\t\t\t\t\t\t\t\t\t\t Elizabeth's Nation" + ANSI_RESET);
+        System.out.println(ANSI_CYAN + "\t\t\t\t\t\t\t\t\t\t\t Tanishq's Nation" + ANSI_RESET);
+
+
+    }
+}
