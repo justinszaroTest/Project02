17b6d333de001d6a111549a437a363f09080e5e5
iyert <iyert@moravian.edu>
2021-04-19 23:27:28 -0400

    Added javadocs for Tanishq's healer, warrior, and wizard.

--- TanishqHealer1.java
+++ TanishqHealer1.java
@@ -1,15 +1,28 @@
-
+/**
+ * Tanishq's class for Healer type.
+ */
 public class TanishqHealer1 extends People
 {
+    /**
+     * Basic Constructor.
+     * @param nation name of nation.
+     * @param tribe name of tribe.
+     * @param lifePoints number of life points.
+     */
     TanishqHealer1(String nation, String tribe, int lifePoints)
     {
         super(nation, tribe, PeopleType.cleric, lifePoints);
         myDescription = "\tTanishq Healer 01";
     }
 
+    /**
+     * The strategy heals 5 for friend and 2 for enemy.
+     * @param otherPerson Other Person who is involved in the encounter
+     * @return a negative number for healing.
+     */
     public int encounterStrategy(People otherPerson)
     {
-        if (!this.getNation().equals(otherPerson.getNation()))
+        if (this.getNation().equals(otherPerson.getNation()))
         {
             return -5;
         }
--- TanishqWarrior1.java
+++ TanishqWarrior1.java
@@ -1,15 +1,29 @@
 
+/**
+ * Tanishq's class for Warrior type.
+ */
 public class TanishqWarrior1 extends People
 {
+    /**
+     * Basic Constructor.
+     * @param nation name of nation.
+     * @param tribe name of tribe.
+     * @param lifePoints number of life points.
+     */
     TanishqWarrior1(String nation, String tribe, int lifePoints)
     {
         super(nation, tribe, PeopleType.warrior, lifePoints);
         myDescription = "\tTanishq Warrior";
     }
 
+    /**
+     * The strategy heals 1 for friend and damages life points / 4 for enemy.
+     * @param otherPerson Other Person who is involved in the encounter
+     * @return negative number if friend, positive if enemy.
+     */
     public int encounterStrategy(People otherPerson)
     {
-        if (!this.getNation().equals(otherPerson.getNation()))
+        if (this.getNation().equals(otherPerson.getNation()))
         {
             return -1;
         }
